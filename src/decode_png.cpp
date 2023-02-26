a9ab77c110e4d2141d332122c5c955b0d64d15a1
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-02-26 18:22:36 +0000

    goodness gracious I think it's successfully read in a png

---
+++ src/decode_png.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "lodepng.h"
+
+int main(int argc, char** argv) {
+  const std::string filename {argv[1]};
+  std::vector<unsigned char> image; // the raw pixels
+  unsigned width, height;
+
+  // decode
+  unsigned error = lodepng::decode(image, width, height, filename);
+  if (error) {
+    std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+  } else {
+    std::cout << "image read successfully: width = " << width << ", height = " << height << std::endl;
+  }
+  return 0;
+}
+
