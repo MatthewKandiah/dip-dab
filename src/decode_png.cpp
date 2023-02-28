2b309e2cd6dd53302c0f149c2dd741e59abac6f2
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-02-28 22:33:39 +0000

    omg I think it works

--- src/decode_png.cpp
+++ src/decode_png.cpp
@@ -15,9 +15,12 @@ int main(int argc, char** argv) {
 
   GreyscaleProcessor greyscaleProcessor = GreyscaleProcessor(decoder.image);
   const std::vector<unsigned char> greyscaleImage = greyscaleProcessor.process();
+  std::cout << "greyscale image produced in memory" << '\n';
   
   const std::string outputFilename {filename + ".grey.png"};
+  std::cout << "expected outputFilename " << outputFilename << '\n'; 
   Encoder encoder = Encoder(outputFilename, greyscaleImage, decoder.width, decoder.height);
+  encoder.encode();
   return 0;
 }
 
