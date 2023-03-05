86ed3a030765a4dc40036d42f3c66f097f5480e8
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-03-05 07:16:38 +0000

    filter wip

--- src/filter.h
+++ src/filter.h
@@ -1,6 +1,13 @@
+#include <vector>
+#include <array>
+
 class Filter {
   const int width, height;
-  Filter(int x, int y):
-    width(x),
-    height(y) {};
+  std::vector<std::vector<int>> data;
+  Filter(std::vector<std::vector<int>> x):
+    data(x),
+    width(x.size()),
+    height(x[0].size()) {}
+
+  // TODO public function to apply filter to a passed in pixel neighborhood, returning new pixel value
 };
