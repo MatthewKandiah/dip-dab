9cc613229657aef13b67783b4b13230d8b6ee961
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-03-07 21:41:25 +0000

    TODO

---
+++ src/mean_blur_processor.h
@@ -0,0 +1,2 @@
+// TODO - take a size, generate a mean blur filter with that height and width
+// then take in an image data (like we get from decoder) make an Image and apply the filter to get the output image data
