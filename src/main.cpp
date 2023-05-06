be3605549348de17a18d585767be90da76618f57
Matthew Kandiah <matthew.kandiah@glean.co>
2023-05-06 16:05:57 +0100

    copy lodepng example code for encoding and decoding png file <-> memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
-#include "lodepng.h"
+#include "lodepng/lodepng.h"
+#include <cstdint>
 #include <iostream>
+#include <string>
+#include <vector>
+
+
+void decodeOneStep(const char *filename, std::vector<unsigned char> &image, unsigned &width, unsigned &height) {
+  unsigned error = lodepng::decode(image, width, height, filename);
+
+  if (error)
+    std::cout << "decoder error " << error << ": " << lodepng_error_text(error)
+              << '\n';
+}
+
+void encodeOneStep(const char *filename, std::vector<unsigned char> &image,
+                   unsigned &width, unsigned &height) {
+  unsigned error = lodepng::encode(filename, image, width, height);
+
+  if (error)
+    std::cout << "encoder error" << error << ": " << lodepng_error_text(error)
+              << '\n';
+}
 
 int main() {
-  std::cout << "hello there\n";
+  const std::string filename = "examples/sunset.png";
+  const std::string outputFilename = "examples/sunset.example.png";
+
+  std::vector<unsigned char> image;
+  unsigned width, height;
+
+  decodeOneStep(filename.c_str(), image, width, height);
+  encodeOneStep(outputFilename.c_str(), image, width, height);
+
   return 0;
 }
