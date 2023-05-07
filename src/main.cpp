218c069bfb4e1dd39509f4d031cc4502bacf8cda
Matthew Kandiah <matthew.kandiah@glean.co>
2023-05-07 08:25:30 +0100

    remove redefinitions from main.cpp and import from headers, failing to build still

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,10 @@
-//#include "imageDecoder.hpp"
-//#include "imageEncoder.hpp"
+#include "imageDecoder.hpp"
+#include "imageEncoder.hpp"
 #include "lodepng/lodepng.h"
 #include <cstdint>
 #include <iostream>
 #include <string>
 #include <vector>
-
-class ImageEncoder {
-public:
-  void encodePng(const std::string &filename, std::vector<std::uint8_t> &image,
-                 std::uint32_t &width, std::uint32_t &height){
-   std::uint32_t error = lodepng::encode(filename.c_str(), image, width, height);
-
-  if (error) {
-    std::cout << "encoder error " << error << ": " << lodepng_error_text(error)
-              << '\n';
-  }
- };
-};
-
-class ImageDecoder {
-public:
-  void decodePng(const std::string &filename, std::vector<std::uint8_t> &image,
-                 std::uint32_t &width, std::uint32_t &height){
-   std::uint32_t error = lodepng::decode(image, width, height, filename.c_str());
-
-  if (error) {
-    std::cout << "decoder error " << error << ": " << lodepng_error_text(error)
-              << '\n';
-  }
- };
-};
 
 int main() {
   const std::string filename = "examples/sunset.png";
