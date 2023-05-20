44a1a615137cbf710c853addc2aba76808a1938a
Matthew Kandiah <matthew.kandiah@glean.co>
2023-05-20 14:25:51 +0100

    formatted

--- src/imageHandler.hpp
+++ src/imageHandler.hpp
@@ -2,14 +2,16 @@
 #define IMAGE_HANDLER_HPP
 
 #include <cstdint>
-#include <vector>
 #include <string>
+#include <vector>
 
 class ImageHandler {
-  public:
-    void decodePng(const std::string &filename, std::vector<std::uint8_t> &image, std::uint32_t &width, std::uint32_t &height);
+public:
+  void decodePng(const std::string &filename, std::vector<std::uint8_t> &image,
+                 std::uint32_t &width, std::uint32_t &height);
 
-    void encodePng(const std::string &filename, std::vector<std::uint8_t> &image, std::uint32_t &width, std::uint32_t &height);
+  void encodePng(const std::string &filename, std::vector<std::uint8_t> &image,
+                 std::uint32_t &width, std::uint32_t &height);
 };
 
 #endif // IMAGE_HANDLER_HPP
