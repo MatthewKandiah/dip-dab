f40a033a116ec49ce4a5919bf88e85cce2d45b29
Matthew Kandiah <matthew.kandiah@glean.co>
2023-05-22 13:14:10 +0100

    refactor - kernel file

--- src/imageHandler.cpp
+++ src/imageHandler.cpp
@@ -1,89 +1,7 @@
 #include "imageHandler.hpp"
 #include "lodepng/lodepng.h"
+#include "kernel.hpp"
 #include <iostream>
-#include <utility>
-
-// TODO - move to its own sensible file
-class Kernel {
-public:
-    Kernel(std::vector<std::vector<std::int32_t> > newData) {
-        data = std::move(newData);
-    }
-
-    std::vector<std::vector<std::int32_t> > data;
-
-    std::size_t width() { return data[0].size(); } // must be odd to be sensibly centred on pixel
-
-    std::size_t height() { return data.size(); } // must be odd to be sensibly centred on pixel
-
-    std::int32_t rightBoundary() {
-        return static_cast<std::int32_t>(0.5 * static_cast<double>(width() - 1));
-    }
-
-    std::int32_t upperBoundary() {
-        return static_cast<std::int32_t>(0.5 * static_cast<double>(height() - 1));
-    }
-
-    std::int32_t leftBoundary() {
-        return -rightBoundary();
-    }
-
-    std::int32_t lowBoundary() {
-        return -upperBoundary();
-    }
-
-    std::int32_t getNormalisationFactor() {
-        auto result = 0;
-        for (auto row: data) {
-            for (auto value: row) {
-                result += value;
-            }
-        }
-        return result;
-    }
-
-    Image apply(const Image &image) {
-        std::vector<Pixel> resultPixelData;
-        for (auto rowIndex = 0; rowIndex < image.data.size(); rowIndex++) {
-            for (auto columnIndex = 0; columnIndex < image.data[0].size(); columnIndex++) {
-                resultPixelData.push_back(applyToPixel(image, columnIndex, rowIndex));
-            }
-        }
-        return Image(resultPixelData, image.width);
-    }
-
-private:
-    Pixel applyToPixel(const Image &image, std::size_t columnIndex, std::size_t rowIndex) {
-        std::int32_t resultR = 0, resultG = 0, resultB = 0, resultA = 0;
-
-        for (auto kernelColumnPosition = leftBoundary();
-             kernelColumnPosition <= rightBoundary(); kernelColumnPosition++) {
-            for (auto kernelRowPosition = lowBoundary(); kernelRowPosition <= upperBoundary(); kernelRowPosition++) {
-                const auto kernelValue = data[kernelRowPosition][kernelColumnPosition];
-                auto imageColumnPosition = static_cast<std::int32_t>(columnIndex + kernelColumnPosition);
-                auto imageRowPosition = static_cast<std::int32_t>(rowIndex + kernelRowPosition);
-                Pixel currentPixel;
-                if (imageColumnPosition < 0 || imageColumnPosition >= image.width || imageRowPosition < 0 ||
-                    imageRowPosition >= image.height) {
-                    // effectively adds an infinite, transparent, black border around the image to handle the edges
-                    currentPixel = Pixel(0, 0, 0, 0);
-                } else {
-                    currentPixel = image.data[imageRowPosition][imageColumnPosition];
-                }
-                resultR += kernelValue * currentPixel.r;
-                resultG += kernelValue * currentPixel.g;
-                resultB += kernelValue * currentPixel.b;
-                resultA += kernelValue * currentPixel.a;
-            }
-        }
-        const auto normalisationFactor = getNormalisationFactor();
-        resultR /= normalisationFactor;
-        resultG /= normalisationFactor;
-        resultB /= normalisationFactor;
-        resultA /= normalisationFactor;
-        return Pixel(resultR, resultG, resultB, resultA);
-    }
-};
 
 void ImageHandler::decodePng(const std::string &filename) {
     std::vector<std::uint8_t> imageData;
@@ -109,8 +27,8 @@ void ImageHandler::encodePng(const std::string &filename) {
 
 void ImageHandler::convertToMonochrome() {
     std::vector<std::uint8_t> resultImageData;
-    for (auto row: image.data) {
-        for (auto pixel: row) {
+    for (const auto &row: image.data) {
+        for (const auto &pixel: row) {
             const auto grey = static_cast<std::uint8_t>(0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b);
             resultImageData.push_back(grey);
             resultImageData.push_back(grey);
@@ -123,8 +41,8 @@ void ImageHandler::convertToMonochrome() {
 
 void ImageHandler::invertImage() {
     std::vector<std::uint8_t> resultImageData;
-    for (auto row: image.data) {
-        for (auto pixel: row) {
+    for (const auto &row: image.data) {
+        for (const auto &pixel: row) {
             resultImageData.push_back(255 - pixel.r);
             resultImageData.push_back(255 - pixel.g);
             resultImageData.push_back(255 - pixel.b);
@@ -136,8 +54,8 @@ void ImageHandler::invertImage() {
 
 void ImageHandler::makeOpaque() {
     std::vector<std::uint8_t> resultImageData;
-    for (auto row: image.data) {
-        for (auto pixel: row) {
+    for (const auto &row: image.data) {
+        for (const auto &pixel: row) {
             resultImageData.push_back(pixel.r);
             resultImageData.push_back(pixel.g);
             resultImageData.push_back(pixel.b);
@@ -148,7 +66,7 @@ void ImageHandler::makeOpaque() {
 }
 
 void ImageHandler::doMeanBlur() {
-    std::vector<int32_t> row = {1,1,1,1,1};
+    std::vector<int32_t> row = {1, 1, 1, 1, 1};
     std::vector<std::vector<int32_t> > kernelData = {row, row, row, row, row};
 
     auto kernel = Kernel(kernelData);
---
+++ src/kernel.hpp
@@ -0,0 +1,88 @@
+#ifndef KERNEL_HPP
+#define KERNEL_HPP
+
+#include "image.hpp"
+#include <vector>
+
+class Kernel {
+public:
+    Kernel(std::vector<std::vector<std::int32_t> > newData) {
+        data = std::move(newData);
+    }
+
+    std::vector<std::vector<std::int32_t> > data;
+
+    std::size_t width() { return data[0].size(); } // must be odd to be sensibly centred on pixel
+
+    std::size_t height() { return data.size(); } // must be odd to be sensibly centred on pixel
+
+    std::int32_t rightBoundary() {
+        return static_cast<std::int32_t>(0.5 * static_cast<double>(width() - 1));
+    }
+
+    std::int32_t upperBoundary() {
+        return static_cast<std::int32_t>(0.5 * static_cast<double>(height() - 1));
+    }
+
+    std::int32_t leftBoundary() {
+        return -rightBoundary();
+    }
+
+    std::int32_t lowBoundary() {
+        return -upperBoundary();
+    }
+
+    std::int32_t getNormalisationFactor() {
+        auto result = 0;
+        for (const auto &row: data) {
+            for (const auto &value: row) {
+                result += value;
+            }
+        }
+        return result;
+    }
+
+    Image apply(const Image &image) {
+        std::vector<Pixel> resultPixelData;
+        for (auto rowIndex = 0; rowIndex < image.data.size(); rowIndex++) {
+            for (auto columnIndex = 0; columnIndex < image.data[0].size(); columnIndex++) {
+                resultPixelData.push_back(applyToPixel(image, columnIndex, rowIndex));
+            }
+        }
+        return Image(resultPixelData, image.width);
+    }
+
+private:
+    Pixel applyToPixel(const Image &image, std::size_t columnIndex, std::size_t rowIndex) {
+        std::int32_t resultR = 0, resultG = 0, resultB = 0, resultA = 0;
+
+        for (auto kernelColumnPosition = leftBoundary();
+             kernelColumnPosition <= rightBoundary(); kernelColumnPosition++) {
+            for (auto kernelRowPosition = lowBoundary(); kernelRowPosition <= upperBoundary(); kernelRowPosition++) {
+                const auto kernelValue = data[kernelRowPosition][kernelColumnPosition];
+                auto imageColumnPosition = static_cast<std::int32_t>(columnIndex + kernelColumnPosition);
+                auto imageRowPosition = static_cast<std::int32_t>(rowIndex + kernelRowPosition);
+                Pixel currentPixel;
+                if (imageColumnPosition < 0 || imageColumnPosition >= image.width || imageRowPosition < 0 ||
+                    imageRowPosition >= image.height) {
+                    // effectively adds an infinite, transparent, black border around the image to handle the edges
+                    currentPixel = Pixel(0, 0, 0, 0);
+                } else {
+                    currentPixel = image.data[imageRowPosition][imageColumnPosition];
+                }
+                resultR += kernelValue * currentPixel.r;
+                resultG += kernelValue * currentPixel.g;
+                resultB += kernelValue * currentPixel.b;
+                resultA += kernelValue * currentPixel.a;
+            }
+        }
+        const auto normalisationFactor = getNormalisationFactor();
+        resultR /= normalisationFactor;
+        resultG /= normalisationFactor;
+        resultB /= normalisationFactor;
+        resultA /= normalisationFactor;
+        return Pixel(resultR, resultG, resultB, resultA);
+    }
+};
+
+#endif
